dfae71568ea4cfb597c25dba46381e7515e8a364
Mondo <mondogao@gmail.com>
2017-06-08 21:23:31 +0800

    feat(pages/index): 增加路由跳转、日期为当天

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -7,16 +7,20 @@ Page({
         desc: '华科景甚好 只是将离别',
         picNum: 100,
         coverSrc: '/assets/cover@2x.png',
-        lunar: '宜行房'
+        lunar: '宜跳舞'
       },
       {
-        title: '风景',
-        desc: '华科景甚好 只是将离别',
-        picNum: 100,
+        title: '人物',
+        desc: '石男私房照',
+        picNum: 98,
         coverSrc: '/assets/cover@2x.png',
         lunar: '宜行房'
       }
-    ]
+    ],
+    date: {
+      month: new Date().getMonth() + 1,
+      day: new Date().getDate(),
+    }
   },
   handleSwiperChange(e) {
     this.setData({
