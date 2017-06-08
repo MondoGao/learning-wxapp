0b9f76587f1fbe688a12c6cbabe842e58601b93a
Mondo <mondogao@gmail.com>
2017-06-08 19:50:31 +0800

    feat(pages/index): 完成数据绑定

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,26 @@
 Page({
   data: {
-    text: '华中大小程序'
+    swiperCurrent: 0,
+    albums: [
+      {
+        title: '风景',
+        desc: '华科景甚好 只是将离别',
+        picNum: 100,
+        coverSrc: '/assets/cover@2x.png',
+        lunar: '宜行房'
+      },
+      {
+        title: '风景',
+        desc: '华科景甚好 只是将离别',
+        picNum: 100,
+        coverSrc: '/assets/cover@2x.png',
+        lunar: '宜行房'
+      }
+    ]
   },
-  onLoad() {
-  
+  handleSwiperChange(e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
   }
 })
