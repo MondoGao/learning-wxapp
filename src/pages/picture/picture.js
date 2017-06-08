1f65089ea69cdbb0ef10909bdb2a83f381784780
Mondo <mondogao@gmail.com>
2017-06-08 22:57:28 +0800

    feat(pages/picture): 完成样式

--- src/pages/picture/picture.js
+++ src/pages/picture/picture.js
@@ -1,3 +1,51 @@
 Page({
-
+  data: {
+    pic: {
+      src: '/assets/cover@2x.png',
+      desc: '我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿',
+      date: '2014-05-20',
+      user: {
+        id: 1,
+        nickname: 'SHINAN'
+      },
+      comments: [
+        {
+          user: {
+            id: 2,
+            nickname: '麦冬',
+          },
+          content: '还是本人比较帅'
+        },
+        {
+          user: {
+            id: 2,
+            nickname: '麦冬',
+          },
+          content: '还是本人比较帅'
+        },
+        {
+          user: {
+            id: 2,
+            nickname: '麦冬',
+          },
+          content: '还是本人比较帅'
+        },
+        {
+          user: {
+            id: 2,
+            nickname: '麦冬',
+          },
+          content: '还是本人比较帅'
+        },
+        {
+          user: {
+            id: 2,
+            nickname: '麦冬',
+          },
+          content: '还是本人比较帅'
+        }
+      ]
+    },
+    
+  }
 })
---
+++ src/pages/picture/picture.json.js
@@ -0,0 +1,3 @@
+module.exports = {
+  navigationBarTitleText: '图片详情'
+}
